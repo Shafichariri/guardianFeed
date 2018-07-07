a4b7595191e9cd87d8e726573a32c44acba562c0
Shafic Hariri <shafic.hariri@keeward.com>
2018-07-07 19:54:27 +0300

    Develop the Articles adapter

---
+++ app/src/main/java/com/shelvz/assignment/adapters/ArticlesAdapter.kt
@@ -0,0 +1,24 @@
+package com.shelvz.assignment.adapters
+
+import android.content.Context
+import com.shelvz.assignment.R
+import com.shelvz.assignment.databinding.ItemArticleBinding
+import com.shelvz.assignment.kit.databinding.DataBoundAbstractAdapter
+import com.shelvz.assignment.kit.databinding.DataBoundBaseViewHolder
+import com.shelvz.assignment.models.Article
+
+class ArticlesAdapter(context: Context, items: MutableList<Article>) :
+        DataBoundAbstractAdapter<Article, ItemArticleBinding>(context, items) {
+
+    override fun getLayoutIdForPosition(position: Int): Int {
+        return R.layout.item_article
+    }
+
+    override fun onViewHolderBound(item: Article, position: Int, binding: ItemArticleBinding, holder: DataBoundBaseViewHolder<Article, ItemArticleBinding>) {
+        binding.test = item.webTitle
+    }
+
+    override fun getLoaderLayoutId(): Int {
+        return R.layout.item_load_more
+    }
+}
