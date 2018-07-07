2fd494bf8720631e7ead413f36caf4a6d1418150
Shafic Hariri <shafic.hariri@keeward.com>
2018-07-07 19:52:13 +0300

    Update the base adapter

--- app/src/main/java/com/shelvz/assignment/kit/base/BaseAdapter.kt
+++ app/src/main/java/com/shelvz/assignment/kit/base/BaseAdapter.kt
@@ -1,8 +1,10 @@
 package com.shelvz.assignment.kit.base
 
 import android.content.Context
+import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import com.shelvz.assignment.kit.databinding.DataBoundBaseViewHolder
 import java.util.*
 
 /**
@@ -14,28 +16,95 @@ abstract class BaseAdapter<T, VH : RecyclerView.ViewHolder>(val context: Context
 
     var onItemClickListener: OnItemClickListener? = null
     var onItemLongClickListener: OnItemLongClickListener? = null
+    private var showLoader: Boolean = false
 
     init {
         //Do on Initialization
     }
 
+    abstract fun getLoaderLayoutId(): Int
+
+    private fun shouldShowLoader(): Boolean {
+        return showLoader
+    }
+
+    fun setShowLoader(visible: Boolean) {
+        when {
+            (showLoader == visible) -> {
+                //Do nothing
+            }
+            visible -> {
+                showLoader = true
+                notifyItemInserted(itemCount - 1)
+            }
+            else -> {
+                showLoader = false
+                notifyItemRemoved(itemCount)
+            }
+        }
+    }
+
+    fun isLoaderPosition(position: Int): Boolean {
+        return shouldShowLoader() && position == itemCount - 1
+    }
+
     override fun getItemCount(): Int {
-        return data.size
+        var itemCount = data.size
+
+        if (shouldShowLoader()) {
+            itemCount++
+        }
+
+        return itemCount
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (isLoaderPosition(position = position)) {
+            getLoaderLayoutId()
+        } else {
+            super.getItemViewType(position)
+        }
     }
 
     override fun onBindViewHolder(holder: VH, position: Int) {
-        if (holder.itemView != null) {
-            if (onItemClickListener != null) {
-                holder.itemView.setOnClickListener { onItemClickListener!!.onItemClick(position, data[position]) }
-            }
+        if (!isLoaderPosition(position = position)) {
+            if (holder.itemView != null) {
+                if (onItemClickListener != null) {
+                    holder.itemView.setOnClickListener {
+                        onItemClickListener!!.onItemClick(position, getDataItem(position))
+                    }
+                }
 
-            if (onItemLongClickListener != null) {
-                holder.itemView.setOnLongClickListener {
-                    onItemLongClickListener!!.onItemLongClick(position, data[position])
-                    return@setOnLongClickListener true
+                if (onItemLongClickListener != null) {
+                    holder.itemView.setOnLongClickListener {
+                        onItemLongClickListener!!.onItemLongClick(position, getDataItem(position))
+                        return@setOnLongClickListener true
+                    }
                 }
             }
+        } else {
+            //Do anything for the loader view
         }
+    }
+
+    /**
+     * Get items safely with regards to the extra custom views
+     * if any above the data or below the data
+     * (but not the load more | its below).
+     * */
+    fun getDataItem(position: Int): T {
+        val dataPosition = getDataRelativePosition(position)
+        return data[dataPosition]
+    }
+
+    open fun getDataRelativePosition(position: Int): Int {
+        var returnPositionValue = position
+
+        //if (isShowingAnyCustomViewBeforeThelastItem()) {
+        //    returnPositionValue--
+        //}
+
+        return returnPositionValue
     }
 
     interface OnItemClickListener {
