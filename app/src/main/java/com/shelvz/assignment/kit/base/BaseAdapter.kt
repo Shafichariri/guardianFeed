7e30fffcc1a460337d3ab0153517f82eb2d79522
Shafic Hariri <shafic.hariri@keeward.com>
2018-07-08 02:07:46 +0300

    Clean up the base adapter imports

--- app/src/main/java/com/shelvz/assignment/kit/base/BaseAdapter.kt
+++ app/src/main/java/com/shelvz/assignment/kit/base/BaseAdapter.kt
@@ -1,10 +1,8 @@
 package com.shelvz.assignment.kit.base
 
 import android.content.Context
-import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import com.shelvz.assignment.kit.databinding.DataBoundBaseViewHolder
 import java.util.*
 
 /**
