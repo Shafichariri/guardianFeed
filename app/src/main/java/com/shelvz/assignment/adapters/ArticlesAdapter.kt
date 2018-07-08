840aca61d00e8121dd7748f5416881b73c2a749f
Shafic Hariri <shafic.hariri@keeward.com>
2018-07-08 16:56:19 +0300

    Update the Articles adapter

--- app/src/main/java/com/shelvz/assignment/adapters/ArticlesAdapter.kt
+++ app/src/main/java/com/shelvz/assignment/adapters/ArticlesAdapter.kt
@@ -1,11 +1,14 @@
 package com.shelvz.assignment.adapters
 
 import android.content.Context
+import android.support.v7.util.DiffUtil
 import com.shelvz.assignment.R
+import com.shelvz.assignment.adapters.DiffUtils.DiffUtilsCallback
 import com.shelvz.assignment.databinding.ItemArticleBinding
 import com.shelvz.assignment.kit.databinding.DataBoundAbstractAdapter
 import com.shelvz.assignment.kit.databinding.DataBoundBaseViewHolder
 import com.shelvz.assignment.models.Article
+
 
 class ArticlesAdapter(context: Context, items: MutableList<Article>) :
         DataBoundAbstractAdapter<Article, ItemArticleBinding>(context, items) {
@@ -22,5 +25,13 @@ class ArticlesAdapter(context: Context, items: MutableList<Article>) :
 
     override fun getLoaderLayoutId(): Int {
         return R.layout.item_load_more
+    }
+
+    override fun update(newItems: List<Article>) {
+        val diffResult = DiffUtil.calculateDiff(DiffUtilsCallback(data, newItems))
+        diffResult.dispatchUpdatesTo(this)
+
+        data.clear()
+        data.addAll(newItems)
     }
 }
