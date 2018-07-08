cace41015d8fead9659d2860d3b1eb13078c1be5
Shafic Hariri <shafic.hariri@keeward.com>
2018-07-08 22:11:06 +0300

    Update the search API request

--- app/src/main/java/com/shelvz/assignment/network/apiServices/SearchServices.kt
+++ app/src/main/java/com/shelvz/assignment/network/apiServices/SearchServices.kt
@@ -1,7 +1,7 @@
 package com.shelvz.assignment.network.apiServices
 
-import com.shelvz.assignment.models.response.APIResponse
 import com.shelvz.assignment.models.Article
+import com.shelvz.assignment.models.response.APIResponse
 import io.reactivex.Flowable
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -15,10 +15,18 @@ interface SearchServices {
         const val SEARCH_ENDPOINT = "/search"
     }
 
+    /**
+     * @param orderBy 'newest' or 'relevance' string
+     * @param dateFrom Dates must be an ISO8601 date or datetime
+     * (e.g. 2010-07-20 or 2010-07-20T10:00:00+05:00). Remember to URL-encode
+     * the '+' if you provide a timezone offset.
+     * */
     @GET("$SEARCH_ENDPOINT")
     fun search(@Query("q") query: String? = null,
                @Query("page") page: Int = 1,
                @Query("show-fields") showFields: String = "thumbnail",
-               @Query("page-size") pageSize: Int = 10): Flowable<APIResponse<Article>>
+               @Query("page-size") pageSize: Int = 10,
+               @Query("order-by") orderBy: String = "newest",
+               @Query("from-date") dateFrom: String? = null): Flowable<APIResponse<Article>>
 
 }
