e4d7e72f4587886bc474a1b4495ce84cb4cb02ed
Shafic Hariri <shafic.hariri@keeward.com>
2018-07-17 16:35:33 +0300

    Update the BaseAdapter abstract implementation (Remove Loader logic)

--- app/src/main/java/com/shelvz/assignment/kit/base/BaseAdapter.kt
+++ app/src/main/java/com/shelvz/assignment/kit/base/BaseAdapter.kt
@@ -1,87 +1,51 @@
 package com.shelvz.assignment.kit.base
 
 import android.content.Context
+import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import com.shelvz.assignment.kit.utils.DefaultDiffUtilsCallback
 import java.util.*
 
 /**
  * Created by shafic on 8/19/17.
  */
 
-abstract class BaseAdapter<T, VH : RecyclerView.ViewHolder>(val context: Context, var data: MutableList<T> = ArrayList()) : RecyclerView.Adapter<VH>() {
+abstract class BaseAdapter<T: BaseModel, VH : RecyclerView.ViewHolder>(val context: Context, var data: MutableList<T> = 
+        ArrayList()) : RecyclerView.Adapter<VH>() {
     private val layoutInflater: LayoutInflater = LayoutInflater.from(context)
 
     var onItemClickListener: OnItemClickListener? = null
     var onItemLongClickListener: OnItemLongClickListener? = null
-    private var showLoader: Boolean = false
 
     init {
         //Do on Initialization
     }
 
-    abstract fun getLoaderLayoutId(): Int
-
-    private fun shouldShowLoader(): Boolean {
-        return showLoader
-    }
-
-    fun setShowLoader(visible: Boolean) {
-        when {
-            (showLoader == visible) -> {
-                //Do nothing
-            }
-            visible -> {
-                showLoader = true
-                notifyItemInserted(itemCount - 1)
-            }
-            else -> {
-                showLoader = false
-                notifyItemRemoved(itemCount)
-            }
-        }
-    }
-
-    fun isLoaderPosition(position: Int): Boolean {
-        return shouldShowLoader() && position == itemCount - 1
-    }
-
     override fun getItemCount(): Int {
-        var itemCount = data.size
-
-        if (shouldShowLoader()) {
-            itemCount++
-        }
-
-        return itemCount
+        return data.size
     }
 
     override fun getItemViewType(position: Int): Int {
-        return if (isLoaderPosition(position = position)) {
-            getLoaderLayoutId()
-        } else {
-            super.getItemViewType(position)
-        }
+        //Needed implementation
+        return super.getItemViewType(position)
     }
 
     override fun onBindViewHolder(holder: VH, position: Int) {
-        if (!isLoaderPosition(position = position)) {
-            if (holder.itemView != null) {
-                if (onItemClickListener != null) {
-                    holder.itemView.setOnClickListener {
-                        onItemClickListener!!.onItemClick(holder.adapterPosition, getDataItem(holder.adapterPosition))
-                    }
-                }
-
-                if (onItemLongClickListener != null) {
-                    holder.itemView.setOnLongClickListener {
-                        onItemLongClickListener!!.onItemLongClick(holder.adapterPosition, getDataItem(holder.adapterPosition))
-                        return@setOnLongClickListener true
-                    }
+        if (holder.itemView != null) {
+            if (onItemClickListener != null) {
+                holder.itemView.setOnClickListener {
+                    onItemClickListener?.onItemClick(holder.adapterPosition, getDataItem(holder.adapterPosition))
                 }
             }
-        } else {
-            //Do anything for the loader view
+
+            if (onItemLongClickListener != null) {
+                holder.itemView.setOnLongClickListener {
+                    onItemLongClickListener?.onItemLongClick(holder.adapterPosition, getDataItem(holder
+                            .adapterPosition))
+                    return@setOnLongClickListener true
+                }
+            }
         }
     }
 
